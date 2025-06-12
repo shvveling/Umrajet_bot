cee0a39bd75a0d05b348b76f837281c574334517
shvveling <95494203+shvveling@users.noreply.github.com>
2025-06-13 02:54:38 +0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# UmraJet Bot Configuration
-
-BOT_TOKEN = "7667056220:AAEc8DwQ0WJrBfVk_yLN8wLWGpxUfRKT-5A"  # Bot tokeningizni shu yerga yozing
-ADMINS = ["vip_arabiy", "V001VB"]  # @ belgisisiz username yozing
